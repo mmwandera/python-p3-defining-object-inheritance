78d11240c4e67a2af411aa02337be31b70b004f6
mmwandera <mmwandera74@gmail.com>
2024-01-18 09:21:18 +0300

    completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
+# Step 2: Defining the Subclass
+# We use Vehicle as an argument for the Car class to note that Car inherits from Vehicle.
 class Car(Vehicle):
-    pass
+
+    # Step 3: Overwriting Inherited Methods
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+print(Car.__bases__)
+# (<class 'vehicle.Vehicle'>,)
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,18 @@
+
+# lib/vehicle.py
+
+# Step 1: Defining the Superclass
+# Instances of Vehicle initialize with a wheel size and number. We also have go() and fill_up_tank() instance methods that describe some common vehicle behavior.
+
 class Vehicle:
-    pass
+    
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
